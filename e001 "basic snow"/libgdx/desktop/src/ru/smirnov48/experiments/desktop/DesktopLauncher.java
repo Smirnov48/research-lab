98c0b7a292f6171be6d7c16627aaba0c77470618
Smirnov48 <smirnov48@gmail.com>
2019-01-01 23:19:58 +0300

    add templete for libgdx

---
+++ e001 "basic snow"/libgdx/desktop/src/ru/smirnov48/experiments/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.smirnov48.experiments.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.smirnov48.experiments.Snowflakes;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Snowflakes(), config);
+	}
+}
