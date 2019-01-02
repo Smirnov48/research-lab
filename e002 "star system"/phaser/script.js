324e9274a1f87e41a808a00e304636c953dd32a4
Smirnov48 <smirnov48@gmail.com>
2019-01-02 13:55:02 +0300

    add star

--- e002 "star system"/phaser/script.js
+++ e002 "star system"/phaser/script.js
@@ -10,19 +10,34 @@ let config = {
 
 let game = new Phaser.Game(config);
 
-function create () {
-	for (let i = 0; i < 300; i++) {
-		let x = Math.random() * 640;
-		let y = - Math.random() * 360;
-		let size = Math.random() * 6;
-		let speed = 1 + size/6;
-	}
+let graphics;
+let starLine;
 
-    graphics = this.add.graphics({ fillStyle: { color: 0xFFFFFF } });
+function drawStar() {
+	for (let i = 0; i < 10; i++) {
+		let angle = Math.random() * Math.PI*2;
+		let r = Math.random() * 20;
+		
+		starLine.x1 = Math.cos(angle) * r + 320;
+		starLine.y1 = Math.sin(angle) * r + 180;
+		starLine.x2 = 320;//Math.cos(angle) * -r + 320;
+		starLine.y2 = 180;//Math.sin(angle) * -r + 180;
+		
+		graphics.strokeLineShape(starLine);
+	}
+}
+
+function create () {
+//	for (let i = 0; i < 300; i++) {
+//	}
+	starLine = new Phaser.Geom.Line(0, 0, 10, 10);
+
+    graphics = this.add.graphics({ fillStyle: { color: 0xFFFFFF }, lineStyle: { width: 1, color: 0xFFFFFF} });
 }
 
 function update () {
 	graphics.clear();
+	drawStar();
 //	for (let snowflake of snowflakes) {
 
 //		let rect = new Phaser.Geom.Rectangle(snowflake.x, snowflake.y, snowflake.size, snowflake.size);
