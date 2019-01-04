60a32173e39197c91fa2f414c261dd333e7b1397
Smirnov48 <smirnov48@gmail.com>
2019-01-04 17:07:52 +0300

    add size and speed for snowflakes

--- e001 "basic snow"/purejs/script.js
+++ e001 "basic snow"/purejs/script.js
@@ -4,12 +4,19 @@ addEventListener("load", function() {
 	let snowflakes = [];
 	
 	function draw(){
-		
-		var x = Math.random() * 640;
-		var y = - Math.random() * 360;
-		
+
 		if (snowflakes.length < 300) {
-			snowflakes.push({'x' : x, 'y' : y});
+			let x = Math.random() * 640;
+			let y = - Math.random() * 360;
+			let size = Math.random() * 6;
+			let speed = 1 + size/6;
+
+			snowflakes.push({
+					'x' : x, 
+					'y' : y, 
+					'speed': speed, 
+					'size': size
+			});
 		}
 		
 		context.fillStyle = "#000";
@@ -17,8 +24,10 @@ addEventListener("load", function() {
 
 		context.fillStyle = "#FFF";
 		for (let snowflake of snowflakes) {
-			snowflake.y++;
-			context.fillRect(snowflake.x, snowflake.y, 5, 5);
+			snowflake.y += snowflake.speed;
+
+			context.fillRect(snowflake.x, snowflake.y, snowflake.size, snowflake.size);
+
 			if (snowflake.y > 360) {
 				snowflake.x = Math.random() * 640;
 				snowflake.y = - Math.random() * 360;
